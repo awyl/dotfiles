a8df213870dae91b3d2c227c777babee5306b796
awyl <awyl@ylast.com>
2022-10-20 22:58:10 -0400

    [nvim] Use gitui in place of lazygit

--- nvim/.config/astronvim/lua/user/mappings.lua
+++ nvim/.config/astronvim/lua/user/mappings.lua
@@ -1,45 +1,30 @@
-return {
-	n = {
-		["<leader>h"] = false,
-		["<leader>uh"] = { ":nohlsearch<CR>", desc = "Turn off search highlight" },
-		-- easy splits
-		["\\"] = { ":split<CR>", desc = "Horizontal split" },
-		["|"] = { ":vsplit<CR>", desc = "Vertical split" },
-		-- resize with arrows
-		["<Up>"] = {
-			function()
-				require("smart-splits").resize_up(2)
-			end,
-			desc = "Resize split up",
-		},
-		["<Down>"] = {
-			function()
-				require("smart-splits").resize_down(2)
-			end,
-			desc = "Resize split down",
-		},
-		["<Left>"] = {
-			function()
-				require("smart-splits").resize_left(2)
-			end,
-			desc = "Resize split left",
-		},
-		["<Right>"] = {
-			function()
-				require("smart-splits").resize_right(2)
-			end,
-			desc = "Resize split right",
-		},
-		-- move lines easier (mac)
-		["˚"] = { ":m .-2<CR>", desc = "Move line up" },
-		["∆"] = { ":m .+1<CR>", desc = "Move line down" },
-	},
-	["i"] = {
-		["∆"] = { "<Esc>:m .+1<CR>gi", desc = "Move line down" },
-		["˚"] = { "<Esc>:m .-2<CR>gi", desc = "Move line up" },
-	},
-	["v"] = {
-		["∆"] = { ":m '>+1<CR>gv`<my`>mzgv`yo`z", desc = "Move lines down" },
-		["˚"] = { ":m '<-2<CR>gv`>my`<mzgv`yo`z", desc = "Move lines up" },
-	},
-}
+local maps = { i = {}, n = {}, v = {}, t = {}, [""] = {} }
+
+-- move turn off highlight to <leader>uh
+maps.n["h"] = false
+maps.n["<leader>uh"] = { ":nohlsearch<CR>", desc = "Turn off search highlight" }
+
+-- easy splits
+maps.n["\\"] = { ":split<CR>", desc = "Horizontal split" }
+maps.n["|"] = { ":vsplit<CR>", desc = "Vertical split" }
+
+-- resize with arrows instead of ctrl arrows because Mac intercepted this binding
+-- for switching workspace
+maps.n["<Up>"] = { function() require("smart-splits").resize_up(2) end, desc = "Resize split up" }
+maps.n["<Down>"] = { function() require("smart-splits").resize_down(2) end, desc = "Resize split down", }
+maps.n["<Left>"] = { function() require("smart-splits").resize_left(2) end, desc = "Resize split left", }
+maps.n["<Right>"] = { function() require("smart-splits").resize_right(2) end, desc = "Resize split right", }
+
+-- move line(s) easier
+maps.n["˚"] = { ":m .-2<CR>", desc = "Move line up" }
+maps.n["∆"] = { ":m .+1<CR>", desc = "Move line down" }
+maps.i["∆"] = { "<Esc>:m .+1<CR>gi", desc = "Move line down" }
+maps.i["˚"] = { "<Esc>:m .-2<CR>gi", desc = "Move line up" }
+maps.v["∆"] = { ":m '>+1<CR>gv`<my`>mzgv`yo`z", desc = "Move lines down" }
+maps.v["˚"] = { ":m '<-2<CR>gv`>my`<mzgv`yo`z", desc = "Move lines up" }
+
+-- use gitui instead of lazygit
+maps.n["<leader>gg"] = { function() astronvim.toggle_term_cmd "gitui" end, desc = "ToggleTerm gitui" }
+maps.n["<leader>tl"] = { function() astronvim.toggle_term_cmd "gitui" end, desc = "ToggleTerm gitui" }
+
+return maps
