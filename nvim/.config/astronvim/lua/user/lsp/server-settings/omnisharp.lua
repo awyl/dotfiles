17d27d30c1e758a6f3bca57e7dbac38c399b2486
awyl <awyl@ylast.com>
2022-10-27 10:24:04 -0400

    [nvim] add omnisharp config reference link

--- nvim/.config/astronvim/lua/user/lsp/server-settings/omnisharp.lua
+++ nvim/.config/astronvim/lua/user/lsp/server-settings/omnisharp.lua
@@ -1,3 +1,4 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/omnisharp.lua
 return {
 	enable_roslyn_analyzers = false,
 	organize_imports_on_format = true,
