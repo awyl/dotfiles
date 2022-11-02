d7a5cdb44fedfd31edbfc36d5d14ab2099c48657
awyl <awyl@ylast.com>
2022-11-02 09:21:07 -0400

    [nvim] adjust neotree hope to lessen cpu load when open

--- nvim/.config/astronvim/lua/user/plugins/neo-tree.lua
+++ nvim/.config/astronvim/lua/user/plugins/neo-tree.lua
@@ -1,8 +1,14 @@
 return {
+	sources = {
+		"filesystem",
+	},
+	git_status_async_options = {
+		batch_delay = 500,
+	},
 	filesystem = {
 		filtered_items = {
 			-- all "hide" just mean "dimmed out"
-			visible = true,
+			visible = false,
 			hide_dotfiles = false,
 			hide_gitignored = true,
 			never_show = {
