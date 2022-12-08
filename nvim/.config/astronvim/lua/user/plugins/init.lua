aaa2a0c09d52a7328181cd2e4b142bf726fd8116
awyl <awyl@ylast.com>
2022-12-08 07:46:33 -0500

    [nvim] remove dap cfg in favor of astronvim's in 2.9

--- nvim/.config/astronvim/lua/user/plugins/init.lua
+++ nvim/.config/astronvim/lua/user/plugins/init.lua
@@ -1,29 +1,29 @@
 return {
-  -- Rust programming
-  ["simrat39/rust-tools.nvim"] = require("user.plugins.rust.rust-tools"),
-  ["Saecki/crates.nvim"] = require("user.plugins.rust.crates"),
+	-- Rust programming
+	["simrat39/rust-tools.nvim"] = require("user.plugins.rust.rust-tools"),
+	["Saecki/crates.nvim"] = require("user.plugins.rust.crates"),
 
-  -- LSP
-  ["hrsh7th/cmp-nvim-lsp-signature-help"] = {
-    after = "nvim-cmp",
-    config = function()
-      astronvim.add_user_cmp_source("nvim_lsp_signature_help")
-    end,
-  },
+	-- LSP
+	["hrsh7th/cmp-nvim-lsp-signature-help"] = {
+		after = "nvim-cmp",
+		config = function()
+			astronvim.add_user_cmp_source("nvim_lsp_signature_help")
+		end,
+	},
 
-  -- Debugging
-  ["folke/trouble.nvim"] = require("user.plugins.trouble"),
-  ["mfussenegger/nvim-dap"] = require("user.plugins.dap"),
+	-- Debugging
+	["folke/trouble.nvim"] = require("user.plugins.trouble"),
+	-- ["mfussenegger/nvim-dap"] = require("user.plugins.dap"),
 
-  -- minis, a set of plugins
-  ["echasnovski/mini.nvim"] = require("user.plugins.mini"),
+	-- minis, a set of plugins
+	["echasnovski/mini.nvim"] = require("user.plugins.mini"),
 
-  -- tasks
-  ["stevearc/overseer.nvim"] = require("user.plugins.overseer"),
+	-- tasks
+	-- ["stevearc/overseer.nvim"] = require("user.plugins.overseer"),
 
-  -- themes
-  ["EdenEast/nightfox.nvim"] = require("user.plugins.nightfox"),
+	-- themes
+	["EdenEast/nightfox.nvim"] = require("user.plugins.nightfox"),
 
-  -- -- beacon, flash cursor after jump
-  -- ["rainbowhxch/beacon.nvim"] = require("user.plugins.beacon"),
+	-- -- beacon, flash cursor after jump
+	-- ["rainbowhxch/beacon.nvim"] = require("user.plugins.beacon"),
 }
