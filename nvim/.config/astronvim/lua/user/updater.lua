ed9703c546d54eb9c6194fd76fe6d7a7fa751bb2
awyl <awyl@ylast.com>
2023-01-11 13:26:49 -0500

    [nvim] pin plugins, there was a breaking change in heirline

--- nvim/.config/astronvim/lua/user/updater.lua
+++ nvim/.config/astronvim/lua/user/updater.lua
@@ -4,10 +4,10 @@ return {
 	version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
 	branch = "nightly", -- branch name (NIGHTLY ONLY)
 	commit = nil, -- commit hash (NIGHTLY ONLY)
-	pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+	pin_plugins = true, -- nil, true, false (nil will pin plugins on stable only)
 	skip_prompts = false, -- skip prompts about breaking changes
 	show_changelog = true, -- show the changelog after performing an update
-	auto_reload = false, -- automatically reload and sync packer after a successful update
+	auto_reload = true, -- automatically reload and sync packer after a successful update
 	auto_quit = false, -- automatically quit the current session after a successful update
 	-- remotes = { -- easily add new remotes to track
 	--   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
