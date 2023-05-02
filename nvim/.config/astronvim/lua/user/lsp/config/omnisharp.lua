b907f1602c252e2cf29539d0436108fb16e7d0f8
awyl <awyl@ylast.com>
2023-05-02 08:31:30 -0400

    [nvim] temp fix for omnisharp token error

--- nvim/.config/astronvim/lua/user/lsp/config/omnisharp.lua
+++ nvim/.config/astronvim/lua/user/lsp/config/omnisharp.lua
@@ -2,4 +2,22 @@
 return {
 	enable_roslyn_analyzers = true,
 	organize_imports_on_format = true,
+
+	-- Need the following until upstream is fixed.
+	-- Issue: https://github.com/AstroNvim/AstroNvim/issues/1847
+	-- Tempfix: https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
+	on_attach = function(client, bufnr)
+		-- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1492605642
+		local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+		for i, v in ipairs(tokenModifiers) do
+			local tmp = string.gsub(v, " ", "_")
+			tokenModifiers[i] = string.gsub(tmp, "-_", "")
+		end
+		local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+		for i, v in ipairs(tokenTypes) do
+			local tmp = string.gsub(v, " ", "_")
+			tokenTypes[i] = string.gsub(tmp, "-_", "")
+		end
+		require("astronvim.utils.lsp").on_attach(client, bufnr)
+	end,
 }
