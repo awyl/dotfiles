17f6985fbc343af84985f3ce6312fdbfb977be55
awyl <awyl@ylast.com>
2023-06-01 17:57:15 -0400

    [nvim] auto closing inactive buffers

---
+++ nvim/.config/astronvim/lua/user/plugins/nvim-early-retirement.lua
@@ -0,0 +1,10 @@
+-- https://github.com/chrisgrieser/nvim-early-retirement
+-- Auto close inactive buffers
+
+return {
+	{
+		"chrisgrieser/nvim-early-retirement",
+		config = true,
+		event = "VeryLazy",
+	},
+}
